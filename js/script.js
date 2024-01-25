29f2f3f142a4c071626a7327c886e077f963cc46
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-25 18:33:24 +0100

    Add the copy to clipboard functionality and refactor the code for more readability

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-const generateRandom = () => {
+// Initialize
+generateRandom();
+
+// Handle click event for random and Copy
+document
+  .querySelector(".switch__regroup")
+  .addEventListener("click", generateRandom);
+document
+  .querySelector(".switch__link")
+  .addEventListener("click", copyToClipboard);
+
+// Generate random functionality
+function generateRandom() {
   fetch("https://api.quotable.io/random")
     .then((response) => response.json())
     .then((data) => {
@@ -16,36 +28,28 @@ const generateRandom = () => {
             `<button class="btn">${data.tags[i]}</button>`
           );
       }
-      function copyToClipboard() {
-        // Get the text from the input field
-        var inputElement = document.querySelector("myInput");
-        inputElement.select();
-
-        // Create a range and select the text
-        var range = document.createRange();
-        range.selectNode(inputElement);
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-
-        // Execute the copy command
-        document.execCommand("copy");
-
-        // Clear the selection
-        window.getSelection().removeAllRanges();
-
-        // Optionally, provide user feedback (e.g., alert or display a message)
-        alert("Text has been copied to clipboard!");
-      }
     })
     .catch((error) => {
       alert(error.message);
     });
-};
+}
 
-// Initialize
-generateRandom();
+// Copy to clipboard functionality
+function copyToClipboard() {
+  const textToCopy = document.querySelector(".card__quote").textContent;
+  const textToCopyWithoutQuote = textToCopy.replace(/"/g, "");
 
-// Handle click event for random
-document
-  .querySelector(".switch__regroup")
-  .addEventListener("click", generateRandom);
+  // Create a temporary input element
+  const tempInput = document.createElement("input");
+  tempInput.value = textToCopyWithoutQuote;
+  document.body.appendChild(tempInput);
+
+  // Select the text and execute the copy command
+  tempInput.select();
+  document.execCommand("copy");
+
+  // Remove the temporary input element
+  document.body.removeChild(tempInput);
+
+  alert("Text copied to clipboard");
+}
